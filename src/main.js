3f858123e1b34baeca5841c53daa4d3e42cc0b99
davie-lee <davie@townfolio.ca>
2019-01-05 20:44:34 -0600

    Added Google Analytics.

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import router from "./router";
 import store from "./store";
 import "./../node_modules/bulma/css/bulma.css";
 import vueHeadful from "vue-headful";
+import VueAnalytics from "vue-analytics";
 
 Vue.component("vue-headful", vueHeadful);
 Vue.config.productionTip = false;
+
+// Google Analytics
+Vue.use(VueAnalytics, {
+  id: "UA-131720166-1"
+});
 
 new Vue({
   router,
