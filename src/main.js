4e3719d684fb577bad847d5b957c28c662776254
davie-lee <davie@townfolio.ca>
2019-01-19 12:42:31 -0600

    Changed Favicon and installed Algolia search package, test version works. Needs frontend cleanup.

--- src/main.js
+++ src/main.js
@@ -8,9 +8,11 @@ import firebase from "./firebase";
 import "./../node_modules/bulma/css/bulma.css";
 import vueHeadful from "vue-headful";
 import VueAnalytics from "vue-analytics";
+import InstantSearch from "vue-instantsearch";
 
 Vue.component("vue-headful", vueHeadful);
 Vue.config.productionTip = false;
+Vue.use(InstantSearch);
 
 // Google Analytics
 Vue.use(VueAnalytics, {
