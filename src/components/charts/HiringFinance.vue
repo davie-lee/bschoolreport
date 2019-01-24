6e9245f3d44a910f28b0952d3e01866294d905d8
davie-lee <davie@townfolio.ca>
2019-01-23 21:22:59 -0600

    Added new GMAC components to Home.vue

---
+++ src/components/charts/HiringFinance.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="column is-6">
+    <div class="card">
+      <div class="content">
+        <vue-highcharts v-if="isDataReady" :options="HiringConsultingChart" ref="areaCharts"></vue-highcharts>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+import VueHighcharts from "vue2-highcharts";
+
+export default {
+  components: {
+    VueHighcharts
+  },
+  props: ["collegeref"],
+  data() {
+    return {
+      isDataReady: false,
+      HiringConsultingChart: {
+        chart: {
+          height: 300,
+          type: "column"
+        },
+        title: {
+          text: "MBA Hiring for Finance"
+        },
+        subtitle: {
+          text: ""
+        },
+        xAxis: {
+          categories: [],
+          tickmarkPlacement: "on",
+          title: {
+            enabled: true,
+            text: "% of Respondents",
+            align: "high"
+          }
+        },
+        yAxis: {
+          title: {
+            enabled: false
+          }
+        },
+        tooltip: {
+          split: true
+        },
+        plotOptions: {
+          column: {
+            dataLabels: {
+              enabled: true,
+              format: "{point.y}%",
+              style: {
+                fontSize: "9px"
+              }
+            }
+          }
+        },
+        legend: {
+          enabled: false
+        },
+        credits: {
+          enabled: false
+        },
+        series: [
+          {
+            name: "Hiring",
+            color: "#54a0ff",
+            data: []
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+    this.getHiringConsultingData();
+  },
+  methods: {
+    getHiringConsultingData: function() {
+      var vm = this;
+      vm.isDataReady = false;
+      var dbRef = firebase.database().ref("gmac/" + this.collegeref);
+
+      dbRef.once("value").then(function(snapshot) {
+        let itr = 0;
+        snapshot.forEach(function(childSnapshot) {
+          var key = childSnapshot.key; // every year
+
+          if (key != "source") {
+            var totalData = childSnapshot.child("Finance").val();
+            vm.$set(vm.HiringConsultingChart.xAxis.categories, itr, key);
+            vm.$set(vm.HiringConsultingChart.series[0].data, itr, totalData);
+            itr++;
+          }
+        });
+        vm.HiringConsultingChart.subtitle.text =
+          "Source: " + snapshot.child("source").val();
+        vm.isDataReady = true;
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.card {
+  padding: 20px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,29 @@
 <template>
   <div>
+    <vue-headful
+      title="MBA Employment Reports - Home"
+      description="Free resource for prospective MBA students"
+    />
     <Selector/>
     <!-- Charts -->
-    <div class="container">Test</div>
+    <div class="container">
+      <h1 class="title is-4">MBA Hiring Trends & Projections</h1>
+      <p class="subtitle is-7">
+        For explanation of these charts, please refer to
+        <a
+          href="https://www.gmac.com/market-intelligence-and-research/market-research/corporate-recruiters-survey"
+          target="new"
+        >GMAC Corporate Recruiters Survey Report</a>.
+      </p>
+      <div class="columns">
+        <HiringConsulting collegeref="hiring_industry"/>
+        <HiringFinance collegeref="hiring_industry"/>
+      </div>
+      <div class="columns">
+        <HiringTech collegeref="hiring_industry"/>
+        <HiringNonprofit collegeref="hiring_industry"/>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,10 +32,18 @@
 import Selector from "@/components/Selector";
 
 // Chart Components
+import HiringConsulting from "../components/charts/HiringConsulting";
+import HiringFinance from "../components/charts/HiringFinance";
+import HiringNonprofit from "../components/charts/HiringNonprofit";
+import HiringTech from "../components/charts/HiringTech";
 
 export default {
   components: {
-    Selector
+    Selector,
+    HiringConsulting,
+    HiringFinance,
+    HiringNonprofit,
+    HiringTech
   }
 };
 </script>
